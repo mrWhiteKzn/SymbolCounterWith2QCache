c6a00fb09a322146d02faf94d8c807fc2a9be072
mrwhitekzn <dianjxrf1>
2018-08-28 22:23:21 +0300

    cache limited, auto cleaning added

--- src/com/foxminded/dmitriy/task5/symbolCounter/SymbolCounter.java
+++ src/com/foxminded/dmitriy/task5/symbolCounter/SymbolCounter.java
@@ -1,15 +1,18 @@
 package com.foxminded.dmitriy.task5.symbolCounter;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SymbolCounter {
-
-    private static final int HASH_SIZE = 4096;
-    private Map<String, Map> cache = new HashMap<>(HASH_SIZE);
+    private static final int MAX_ENTRIES = 128;
+    private LinkedHashMap<String, Map> cache = new LinkedHashMap<String, Map>(MAX_ENTRIES) {
+        @Override
+        protected boolean removeEldestEntry(Map.Entry eldest) {
+            return size() >= MAX_ENTRIES;
+        }
+    };
 
     public Map count(String input) {
         if (!cache.containsKey(input)) {
